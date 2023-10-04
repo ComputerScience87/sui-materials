e13bebf1722862916dd1fdf37a67b7dc5bafaaec
Micheal Hannan <micheal.hannan@procore.com>
2023-10-04 10:18:00 -0500

    Chapter 7: Making the actual ChallengeView for chapter example of active usage of containers.

--- 07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/Practice/ChallengeView.swift
+++ 07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/Practice/ChallengeView.swift
@@ -33,18 +33,39 @@
 import SwiftUI
 
 struct ChallengeView: View {
-  var body: some View {
-      HStack(alignment: .firstTextBaseline) {
-          Text("Welcome to Kuchi").font(.caption)
-          Text("Welcome to Kuchi").font(.title)
-          Button(action: {}, label: { Text("OK").font(.body) })
-      }
-  }
+    let challengeTest: ChallengeTest
+
+    @State var showAnswers = false
+
+    var body: some View {
+        VStack {
+            Button {
+                self.showAnswers.toggle()
+            } label: {
+                QuestionView(question: challengeTest.challenge.question)
+                    .frame(height: 300)
+            }
+
+            if showAnswers {
+                Divider()
+                ChoicesView(challengeTest: challengeTest)
+                    .frame(height: 300)
+                    .padding()
+            }
+        }
+    }
 }
 
 
 struct ChallengeView_Previews: PreviewProvider {
-  static var previews: some View {
-    return ChallengeView()
-  }
+    static let challengeTest = ChallengeTest(
+        challenge: Challenge(
+            question: "Sample Question",
+            pronunciation: "Onegai shimasu",
+            answer: "Please"),
+        answers: ["Thank You", "Hello", "Goodbye"])
+
+    static var previews: some View {
+        return ChallengeView(challengeTest: challengeTest)
+    }
 }
