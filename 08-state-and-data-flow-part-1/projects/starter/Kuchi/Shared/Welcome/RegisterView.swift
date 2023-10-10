4d419b851bc1fa635af01ac8b35558ff09821b76
Micheal Hannan <micheal.hannan@procore.com>
2023-10-10 11:36:32 -0500

    Chapter 8: Work up to binding the text field with the name property.

--- 08-state-and-data-flow-part-1/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
+++ 08-state-and-data-flow-part-1/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
@@ -33,84 +33,111 @@
 import SwiftUI
 
 struct RegisterView: View {
-  @EnvironmentObject var userManager: UserManager
-  @ObservedObject var keyboardHandler: KeyboardFollower
-  
-  init(keyboardHandler: KeyboardFollower) {
-    self.keyboardHandler = keyboardHandler
-  }
-  
-  var body: some View {
-    VStack {
-      Spacer()
-      
-      WelcomeMessageView()
-      
-      TextField("Type your name...", text: $userManager.profile.name)
-        .bordered()
-      
-      HStack {
-        Spacer()
-        Text("\(userManager.profile.name.count)")
-          .font(.caption)
-          .foregroundColor(
-            userManager.isUserNameValid() ? .green : .red)
-          .padding(.trailing)
-      }
-      .padding(.bottom)
-      
-      HStack {
-        Spacer()
-        
-        Toggle(isOn: $userManager.settings.rememberUser) {
-          Text("Remember me")
-            .font(.subheadline)
-            .foregroundColor(.gray)
-        }
-        .fixedSize()
-      }
-      
-      Button(action: self.registerUser) {
-        HStack {
-          Image(systemName: "checkmark")
-            .resizable()
-            .frame(width: 16, height: 16, alignment: .center)
-          Text("OK")
-            .font(.body)
-            .bold()
-        }
-      }
-      .bordered()
-      .disabled(!userManager.isUserNameValid())
-      
-      Spacer()
+    @ObservedObject var keyboardHandler: KeyboardFollower
+    @State var name: String = ""
+
+    init(keyboardHandler: KeyboardFollower) {
+        self.keyboardHandler = keyboardHandler
     }
-    .padding(.bottom, keyboardHandler.keyboardHeight)
-    .edgesIgnoringSafeArea(keyboardHandler.isVisible ? .bottom : [])
-    .padding()
-    .background(WelcomeBackgroundImage())
-  }
+
+    var body: some View {
+        VStack {
+            TextField("Type your name...", text: $name)
+                .bordered()
+            Text(name)
+        }
+        .padding(.bottom, keyboardHandler.keyboardHeight)
+        .edgesIgnoringSafeArea(keyboardHandler.isVisible ? .bottom : [])
+        .padding()
+        .background(WelcomeBackgroundImage())
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView(keyboardHandler: KeyboardFollower())
+    }
+}
+
+/*struct RegisterView: View {
+    @EnvironmentObject var userManager: UserManager
+    @ObservedObject var keyboardHandler: KeyboardFollower
+
+    init(keyboardHandler: KeyboardFollower) {
+        self.keyboardHandler = keyboardHandler
+    }
+
+    var body: some View {
+        VStack {
+            Spacer()
+
+            WelcomeMessageView()
+
+            TextField("Type your name...", text: $userManager.profile.name)
+                .bordered()
+
+            HStack {
+                Spacer()
+                Text("\(userManager.profile.name.count)")
+                    .font(.caption)
+                    .foregroundColor(
+                        userManager.isUserNameValid() ? .green : .red)
+                    .padding(.trailing)
+            }
+            .padding(.bottom)
+
+            HStack {
+                Spacer()
+
+                Toggle(isOn: $userManager.settings.rememberUser) {
+                    Text("Remember me")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                .fixedSize()
+            }
+
+            Button(action: self.registerUser) {
+                HStack {
+                    Image(systemName: "checkmark")
+                        .resizable()
+                        .frame(width: 16, height: 16, alignment: .center)
+                    Text("OK")
+                        .font(.body)
+                        .bold()
+                }
+            }
+            .bordered()
+            .disabled(!userManager.isUserNameValid())
+
+            Spacer()
+        }
+        .padding(.bottom, keyboardHandler.keyboardHeight)
+        .edgesIgnoringSafeArea(keyboardHandler.isVisible ? .bottom : [])
+        .padding()
+        .background(WelcomeBackgroundImage())
+    }
 }
 
 // MARK: - Event Handlers
 extension RegisterView {
-  func registerUser() {
-    if userManager.settings.rememberUser {
-      userManager.persistProfile()
-    } else {
-      userManager.clear()
+    func registerUser() {
+        if userManager.settings.rememberUser {
+            userManager.persistProfile()
+        } else {
+            userManager.clear()
+        }
+
+        userManager.persistSettings()
+        userManager.setRegistered()
     }
-    
-    userManager.persistSettings()
-    userManager.setRegistered()
-  }
 }
 
 struct RegisterView_Previews: PreviewProvider {
-  static let user = UserManager(name: "Ray")
-  
-  static var previews: some View {
-    RegisterView(keyboardHandler: KeyboardFollower())
-      .environmentObject(user)
-  }
-}
+    static let user = UserManager(name: "Ray")
+
+    static var previews: some View {
+        RegisterView(keyboardHandler: KeyboardFollower())
+            .environmentObject(user)
+    }
+}*/
